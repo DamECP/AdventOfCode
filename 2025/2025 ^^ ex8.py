0bc0d0b1b01ce4ed1045389f03d2072eaa60054c
DamECP <damien.depaepe@gmail.com>
2025-12-23 01:34:51 +0100

    2025 - ex8

---
+++ 2025/2025 ^^ ex8.py
@@ -0,0 +1,85 @@
+from itertools import combinations
+import numpy as np
+from math import prod
+
+with open("ex8.txt") as exercise:
+    data = [
+        tuple(int(n) for n in line.strip().split(",")) for line in exercise.readlines()
+    ]
+
+
+class Box:
+    def __init__(self, x, y, z):
+        self.array = np.array([x, y, z])
+        self.closest = None
+        self.distance = None
+
+
+boxes = [Box(x, y, z) for x, y, z in data]
+
+# gathers closest pairs and their distance as tuples
+pairs = [
+    (n1, n2, np.linalg.norm(n1.array - n2.array)) for n1, n2 in combinations(boxes, 2)
+]
+
+# sort them by distance
+pairs.sort(key=lambda b: b[2])
+
+
+def answer_1(n):
+    # n is part of the instructions
+    shortest_pairs = pairs[:n]
+
+    circuits = []
+
+    for b1, b2, dist in shortest_pairs:
+        # checks of one of the values is in a circuit already
+        overlap = [c for c in circuits if b1 in c or b2 in c]
+
+        # if not, build its own one
+        if not overlap:
+            circuits.append(set([b1, b2]))
+
+        # or merge b1, b2 and the current circuit to avoid doubles
+        else:
+            merged = set([b1, b2])
+            for c in overlap:
+                merged |= c
+                circuits.remove(c)
+            circuits.append(merged)
+
+    # get the 3 biggest circuits
+    sizes = sorted([len(c) for c in circuits], reverse=True)[:3]
+    # use the formula
+    return prod(sizes)
+
+
+print(answer_1(1000))
+
+
+def answer_2():
+    # n is part of the instructions
+
+    circuits = []
+
+    for b1, b2, dist in pairs:
+        # checks of one of the values is in a circuit already
+        overlap = [c for c in circuits if b1 in c or b2 in c]
+
+        # if not, build its own one
+        if not overlap:
+            circuits.append(set([b1, b2]))
+
+        # or merge b1, b2 and the current circuit to avoid doubles
+        else:
+            merged = set([b1, b2])
+            for c in overlap:
+                merged |= c
+                circuits.remove(c)
+            circuits.append(merged)
+
+        if len(circuits) == 1 and len(circuits[0]) == len(boxes):
+            return b1.array[0] * b2.array[0]
+
+
+print(answer_2())
